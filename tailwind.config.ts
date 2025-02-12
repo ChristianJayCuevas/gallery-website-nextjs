e95b0a75173aa8387bf4d1c0da6b917e9a21d400
Christian Jay Cuevas <christianjaycuevas@gmail.com>
2025-02-12 14:09:27 +0800

    Welcome Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/app/**/*.{js,ts,jsx,tsx}', // Scan app files
+    './src/components/**/*.{js,ts,jsx,tsx}', // Scan components folder
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      boxShadow:{
+        custom: '6px 6px 8px rgba(0, 0, 0, 0.6), -3px -3px 5px rgba(255, 255, 255, 0.1)',
+      }
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      "light", "dark", "cupcake", "lemonade", // Add the lemonade theme here
+    ],
+    // Set default theme to "lemonade" to reinforce it
+    darkTheme: "dark",
+  },
+};
+
+export default config;
